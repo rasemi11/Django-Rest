9b124bef8e643b61d81352fd88b8cc1565ef1922
rasemi11 <105454419+rasemi11@users.noreply.github.com>
2022-05-15 06:55:51 -0700

    reload test data

---
+++ reload_test_data.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+conn = sqlite3.connect('db.sqlite3')
+c = conn.cursor()
+
+c.execute("DELETE FROM car_api_app_carbrand")
+c.execute("DELETE FROM car_api_app_carmodel")
+c.execute("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='car_api_app_carbrand';")
+c.execute("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='car_api_app_carmodel';")
+
+
+c.execute("INSERT INTO car_api_app_carbrand "
+          "(name, created_at, deleted_at)"
+          "VALUES('Dacia', 'NOW', NULL), "
+          "('VW', 'NOW', NULL),"
+          "('Alfa', 'NOW', NULL),"
+          "('Renault', 'NOW', NULL)")
+conn.commit()
+
+c.execute("INSERT INTO car_api_app_carmodel "
+          "(name, created_at, deleted_at, car_brand_id) "
+          "VALUES('Logan', 'NOW', NULL,  1),('Docker', 'NOW', NULL, 1),"
+          "('Polo', 'NOW', NULL, 2),"
+          "('Pasat', 'NOW', NULL, 2),"
+          "('Golf', 'NOW', NULL, 2),"
+          "('156', 'NOW', NULL, 3),"
+          "('133', 'NOW', NULL, 3),"
+          "('Clio', 'NOW', NULL, 4),"
+          "('Megane', 'NOW', NULL, 4)")
+conn.commit()
+
+conn.close()
